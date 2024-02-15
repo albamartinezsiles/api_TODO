1fdc4dba9fc48523fa74fe6674225fb321b84037
Alba <dok-do@hotmail.es>
2024-02-15 14:54:24 +0100

    mitad de la validacion de errores

---
+++ db.js
@@ -0,0 +1,48 @@
+require('dotenv').config(); //para que pueda leer dotenv y vea las claves
+const postgres = require("postgres");
+
+function conectar(){ // esta funcion retorna la conexion
+    return postgres({
+        host : "process.env.DB_HOST",
+        database : "process.env.DB_NAME",
+        user : "process.env.DB_USER",
+        password : "process.env.DB_PASSWORD"
+    });
+}
+
+function getTareas(){ 
+    return new Promise(async (fulfill,reject) => { 
+
+        let conexion = conectar(); 
+
+        try{
+            let tareas = await conexion`SELECT * FROM tareas`;
+
+            conexion.end();
+
+            fulfill(tareas); 
+
+        }catch(error){
+            reject({error : "error en base de datos"});
+        }
+    });
+}
+
+function crearTarea({tarea}){ 
+    return new Promise(async (fulfill, reject) => { 
+        let conexion = conectar(); 
+
+        try{
+            let [{id}] = await conexion (`INSERT INTO tareas (tarea) VALUES (${tarea}) RETURNING id`); //la tarea está entre paréntesis porque es el campo que quiero escribir
+
+            conexion.end();
+
+            fulfill(id); 
+
+        }catch(error){
+            reject({error : "error en base de datos"});
+        }
+    });
+}
+
+module.exports = {getTareas,crearTarea};
--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const express = require("express");
 require('dotenv').config();
 const servidor = express();
+const {getTareas,crearTarea} = require ("./db.js"); //traete la funcion getTareas
+const {json} = require ("body-parser");
+
+servidor.use(json());
 
 servidor.use("/pruebas", express.static ("./pruebas_api")); //quiero servir estos ficheros estáticos
 
-servidor.get("/api-todo", (peticion,respuesta) => {
-    return respuesta.send("Esto es el método get");
-    
+servidor.get("/api-todo", async (peticion,respuesta) => {
+   try{ 
+        conectar();
+        let tareas = await getTareas(); 
+
+        respuesta.json(tareas);
+   }catch(error){
+
+        respuesta.status(500);
+        
+        respuesta.json(error); //para que lo convierta en un string y lo envíe. Esto es para que pase a texto basicamente
+
+   }
+        
 });
 
-servidor.post("/api-todo", (peticion,respuesta) => {
-    return respuesta.send("Esto es el método post");
+servidor.post("/api-todo/crear", async (peticion,respuesta,siguiente) => {
+    let {tarea} = peticion.body; //LC CLAVE ESTA AQUI. USAMOS { a : x } es lo que nos va a mandar el BACK. Lo esta desectructurando de un objeto que se supone que esta en la bd. Si el usuario luego no pone una tarea este valor será undefined
+    if(tarea && tarea.trim() != ""){
+        return respuesta.send("Esto es el método post");
+    }
+    siguiente("no me enviaste tarea");
+    //throw "no me enviaste tarea"; //cuando hacemos un throw inmediatamente aterrizamos en ERROR, el último middleware que hemos hecho para responder errores
+
 });
 
 servidor.put("/api-todo", (peticion,respuesta) => {
@@ -23,6 +44,10 @@ servidor.delete("/api-todo", (peticion,respuesta) => {
 
 servidor.use((peticion,respuesta) => { //cualquier cosa que no encaje va a error not found!
     respuesta.json ({ error : "not found" });
-})
+});
+
+servidor.use((error, peticion,respuesta,siguiente) => {
+    respuesta.send("...error");
+});
 
 servidor.listen(process.env.PORT);
