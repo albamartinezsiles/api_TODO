927ec63df6f256dc0e6f0d45d030957811680356
Alba <dok-do@hotmail.es>
2024-02-15 12:49:09 +0100

    metodos configurados

--- index.js
+++ index.js
@@ -2,5 +2,27 @@ const express = require("express");
 require('dotenv').config();
 const servidor = express();
 
+servidor.use("/pruebas", express.static ("./pruebas_api")); //quiero servir estos ficheros estáticos
+
+servidor.get("/api-todo", (peticion,respuesta) => {
+    return respuesta.send("Esto es el método get");
+    
+});
+
+servidor.post("/api-todo", (peticion,respuesta) => {
+    return respuesta.send("Esto es el método post");
+});
+
+servidor.put("/api-todo", (peticion,respuesta) => {
+    return respuesta.send("Esto es el método put");
+});
+
+servidor.delete("/api-todo", (peticion,respuesta) => {
+    return respuesta.send("Esto es el método delete");
+});
+
+servidor.use((peticion,respuesta) => { //cualquier cosa que no encaje va a error not found!
+    respuesta.json ({ error : "not found" });
+})
 
 servidor.listen(process.env.PORT);
